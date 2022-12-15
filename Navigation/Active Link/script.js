cb105fef815fd981201ae1c91d435a18e09b6f35
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2022-12-15 08:07:41 +0500

    Navigating through different pages with active link section added to the directory

---
+++ Navigation/Active Link/script.js
@@ -0,0 +1,8 @@
+// Setup active link stuff
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').
+forEach(link => {
+    if(link.href.includes(`${activePage}`)) {
+        link.classList.add('active');
+    }
+});
