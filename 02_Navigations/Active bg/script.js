6534f4cad87360f8b842f3efc44b869df94374db
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2023-08-21 12:35:17 +0500

    Active background navigation updated

--- 02_Navigations/Active bg/script.js
+++ 02_Navigations/Active bg/script.js
@@ -1,18 +1,18 @@
 // JavaScript Code
-{
-    const activePage = window.location.pathname;
-    const navLinks = document.querySelectorAll('ul li').
-    forEach(link => {
-        // apply CSS class
-        if(link.href.includes(`${activePage}`)) {
-            link.classList.add('active');
-        }
-    });
-}
-
-// // jQuery Code
 // {
-//     $(document).on('click', 'ul li', function() {
-//         $(this).addClass('active').siblings().removeClass('active');
+//     const activePage = window.location.pathname;
+//     const navLinks = document.querySelectorAll('ul li').
+//     forEach(link => {
+//         // apply CSS class
+//         if(link.href.includes(`${activePage}`)) {
+//             link.classList.add('active');
+//         }
 //     });
 // }
+
+// // jQuery Code
+{
+    $(document).on('click', 'ul li', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+}
