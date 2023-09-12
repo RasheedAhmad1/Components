7b05b1c9956aa75e320e80d305bf57c1c0992cb7
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2023-09-12 11:12:11 +0500

    Tab design 3 added

--- Mini-Projects/tab-design/tab2.js
+++ Mini-Projects/tab-design/tab2.js
@@ -2,40 +2,40 @@ const tabsContainer = document.querySelector(".tabs-container");
 
 const tabsList = tabsContainer.querySelector("ul");
 const tabButtons = tabsList.querySelectorAll("a");
-const tabPanels = tabsContainer.querySelector(".tabs__panels > div");
+const tabPanels = tabsContainer.querySelectorAll(".tabs__panels > div");
 
 console.log(tabButtons);
 
 tabButtons.forEach((tab, index) => {
     if (index === 0) {
-
+        // something
     } else {
-        tabPanels[index].setAttribute("hidden", "true");
+        tabPanels[index].setAttribute("hidden", "");
     }
 });
 
 tabsContainer.addEventListener("click", (e) => {
     // grab the link
     const clickedTab = e.target.closest("a");
-    // if the link does clicked, do nothing
+    // if the link is'nt clicked, do nothing
     if (!clickedTab) return;
 
     // console.log(clickedTab);
 
     e.preventDefault();
 
-    switchTab(clickedTab);
+    // switchTab(clickedTab);
 });
 
-tabsList.setAttribute("role", "tablist");
+// tabsList.setAttribute("role", "tablist");
 
-function switchTab(newTab) {
-    const activePanelId = clickedTab.getAttribute("href");
-    const activePanel = tabsContainer.querySelector(activePanelId);
+// function switchTab(newTab) {
+//     const activePanelId = clickedTab.getAttribute("href");
+//     const activePanel = tabsContainer.querySelector(activePanelId);
 
-    // loop through the panels
-    tabPanels.forEach((panel) => {
-        panel.setAttribute("hidden", true);
-    });
-    activePanel.removeAttribute("hidden", false);
-}
+//     // loop through the panels
+//     tabPanels.forEach((panel) => {
+//         panel.setAttribute("hidden", true);
+//     });
+//     activePanel.removeAttribute("hidden", false);
+// }
